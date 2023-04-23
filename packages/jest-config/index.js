b7e5df5768892ab9dc4fcd6654676cf5d7704ad4
Vinicius Campos <vinicamposdev@gmail.com>
2023-04-23 11:52:35 -0300

    build: remove jest config text

--- packages/jest-config/index.js
+++ packages/jest-config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     "^@my-monorepo/(.*)$": "<rootDir>/../$1/src",
   },
   // Use esbuild to transpile TypeScript files on the fly.
-  coverageReporters: ["text", "cobertura", "lcov", "html"],
+  coverageReporters: ["cobertura", "lcov", "html"],
   transform: {
     "^.+\\.tsx?$": "esbuild-jest",
   },
